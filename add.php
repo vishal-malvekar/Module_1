ffe0d4e264d961a90615920ff424f52944234c76
vishal-malvekar <95217418+vishal-malvekar@users.noreply.github.com>
2024-09-26 16:28:46 +0530

    Update add.php

--- add.php
+++ add.php
@@ -66,7 +66,7 @@ function validateFormData() {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Add Profile</title>
+    <title>Vishal Malvekar</title>
 </head>
 
 <body>
